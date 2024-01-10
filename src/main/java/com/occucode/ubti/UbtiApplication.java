b0ae3efbac48e29008b8ea0d3ba237311052d91f
subin195-09 <k_subin@naver.com>
2024-01-10 20:48:30 +0900

    start : )

---
+++ src/main/java/com/occucode/ubti/UbtiApplication.java
@@ -0,0 +1,13 @@
+package com.occucode.ubti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/occucode/ubti/UbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.occucode.ubti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
